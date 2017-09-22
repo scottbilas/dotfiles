0d8b5e73a41e7ead5c1ad73725071d315bae10cf
Scott Bilas <scottbilas@gmail.com>
2017-09-22 21:54:28 +0200

    iterate

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -22,16 +22,32 @@ set -x ELINKS_CONFDIR ~/.config/elinks
 set -U fish_user_paths ~/bin ~/dotfiles/scripts
 
 # bobthefish config
-set -g theme_date_format "+%a %H:%M:%S"
-set -g theme_display_vi no
+set -g theme_display_git_untracked yes
 set -g theme_display_git_ahead_verbose yes
+set -g theme_git_worktree_support no
 set -g theme_display_vagrant yes
+set -g theme_display_docker_machine yes
 set -g theme_display_hg yes
+set -g theme_display_virtualenv yes
+set -g theme_display_ruby no
+set -g theme_display_user no
+set -g theme_display_vi no
+set -g theme_display_date yes
 set -g theme_display_cmd_duration yes
-set -g theme_git_worktree_support yes
-set -g theme_nerd_fonts yes
 set -g theme_title_display_process yes
+set -g theme_title_display_path no
+set -g theme_title_display_user yes
+set -g theme_title_use_abbreviated_path no
+set -g theme_date_format "+%H:%M"
+set -g theme_avoid_ambiguous_glyphs yes
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+set -g default_user your_normal_user
 set -g theme_color_scheme zenburn
+set -g fish_prompt_pwd_dir_length 3
+set -g theme_project_dir_length 3
+set -g theme_newline_cursor yes
 
 # dircolors
 eval (dircolors -c ~/.ghq/github.com/seebi/dircolors-solarized/dircolors.256dark)
