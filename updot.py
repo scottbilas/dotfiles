8fda0bde0e23f360e3e1df2a5ce4aaebd849faa3
Scott Bilas <scottbilas@gmail.com>
2018-01-18 07:31:05 -0800

    Silly fix

--- updot.py
+++ updot.py
@@ -29,8 +29,11 @@ ln('~/dotfiles/private/ssh/authorized_keys', '~/.ssh/authorized_keys')
 
 ln('~/.config/tmux/tmux.conf', '~/.tmux.conf') # tmux refuses to support xdg (https://github.com/tmux/tmux/issues/142)
 ln('~/.config/pdb/pdbrc.py', '~/.pdbrc.py')    # pdbpp uses fancycompleter which hard codes ~/<configname> and doesn't do xdg
-ln(f'{PROJ}/unity-meta', '~/unity- X:os.name == 'nt'
-    touchPOSIX . not WINDOWS
+
+ln(f'{PROJ}/unity-meta', '~/unity-meta')
+
+if platform.POSIX:
+    touch('~/.hushlogin')
 
 if platform.TERMUX:
     ln('~/.config/termux', '~/.termux')
