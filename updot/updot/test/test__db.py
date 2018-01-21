3085b26c47c4fc03534530254c7da410bcd85a47
Scott Bilas <scottbilas@gmail.com>
2018-01-21 04:52:18 -0800

    Improved tests to check that the version increments properly

--- updot/updot/test/test__db.py
+++ updot/updot/test/test__db.py
@@ -1,5 +1,7 @@
 import json
+
 import pytest
+
 from updot import _db
 
 # note that all modules we want to (directly or indirectly) use the fake filesystem need to be
@@ -12,30 +14,35 @@ from updot import _db
 # ^^ TODO: may need a pylint plugin to bless reusing fixture names as params (a pytest design
 #          feature) to avoid disabling this warning entirely
 
+
 @pytest.fixture
-def db(fs): # pylint: disable=unused-argument
+def db(fs):  # pylint: disable=unused-argument
     with _db._Db() as the_db:
         yield the_db
 
-def test__new_db__is_created_with_version_1(db):
-    assert db.version == 1
 
 @pytest.mark.usefixtures('fs')
-def test__new_db__is_saved_with_version_2():
-    with _db._Db() as the_db: # pylint: disable=protected-access
-        db_path = the_db.path
+def test__db_open_close__increments_version():
 
-    with open(db_path) as file:
-        db_json = json.load(file)
+    db_path = '~/db.json'
 
-    assert db_json['_default']['1']['version'] == 2
+    def version_from_json():
+        with open(db_path) as file:
+            db_json = json.load(file)
+        return db_json['_default']['1']['version']
 
-    # TODO:
-    # move global singleton stuff into a simple accessor (continue to use atexit)
-    # unit test the db class directly in this file, probably with a fixture that inherits 'fs' and also does the `with` thing on the db class
-    # make a fixture (that inherits 'fs') that mocks the singleton accessor, and use this from test_ln etc.
-    # the idea is that the singleton accessor is never called in any test code, because we're always either
-    #  mocking it to be reusable, or we're using the class directly.
+    with _db._Db(db_path) as the_db:
+        assert the_db.version == 1
+    assert version_from_json() == 2
+
+    with _db._Db(db_path):
+        pass
+    assert version_from_json() == 3
+
+    with _db._Db(db_path):
+        pass
+    assert version_from_json() == 4
+
 
 if __name__ == "__main__":
     pytest.main(__file__)
