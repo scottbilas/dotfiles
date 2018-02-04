e49b2e2cba52479dd3d565a266d836b35cd7d21b
Scott Bilas <scottbilas@gmail.com>
2018-02-04 18:35:20 +0100

    Copied in (then immediately put on pause to work on macros) a bit of my main updot.py to start using for integration tests.

---
+++ updot/updot/test/test_ln_int.py
@@ -0,0 +1,58 @@
+# this file contains high level integration tests for `ln`
+
+#import os
+
+#import pytest
+#from pytest import raises
+
+#from updot import _db, exceptions, links, platform
+#from updot.links import ln
+#from testutils import HOME, expand
+
+
+#def mkdir(fs, path):
+#    fs.create_dir(expand('~/bin').exp)
+
+"""
+def test__basic_integration_scenario__succeeds(fs):
+
+    mkdir('~/bin')
+    mkdir('~/go/bin')
+    mkdir('~/.ssh')
+
+    PROJ = '~/proj'
+
+    #|
+
+    ln('~/dotfiles/config', '~/.config')
+    ln('~/Common/Private', '~/dotfiles/private')
+    ln('~/dotfiles/private/ssh/authorized_keys', '~/.ssh/authorized_keys')
+    ln('~/.config/tmux/tmux.conf', '~/.tmux.conf', if_app='tmux')
+    ln('~/.config/pdb/pdbrc.py', '~/.pdbrc.py', if_app='python')
+    ln('~/.config/hyper/hyper.js', '~/.hyper.js', if_app='hyper')
+    ln('~/dotfiles/special/zsh/zshenv', '~/.zshenv')
+    ln('~/.config/termux', '~/.termux')
+    ln('~/storage/shared/Sync/Common', '~/Common')
+    ln('~/dotfiles/special/cygwin/profile', '~/.profile')
+    ln('~/dotfiles/special/cygwin/minttyrc', '~/.minttyrc')
+    ln('~/.config/git/config-windows', '~/.gitconfig')
+    ln('~/.config/hg/hgrc', '~/.hgrc')
+    ln('~/.config/omnisharp', '~/.omnisharp')
+    ln('~/dotfiles/private/openvpn/config', '~/OpenVPN/config')
+    ln('~/Common/_Settings/gimp-2.8',                  '.gimp-2.8')
+    ln('~/Common/_Settings/Ssh',                       '.ssh')
+    ln('~/Common/_Settings/minttyrc.txt',              '.minttyrc')
+    ln('~/Common/Visual Studio 2017',                  'Documents/Visual Studio 2017')
+    ln('~/Common/WindowsPowerShell',                   'Documents/WindowsPowerShell')
+
+    if platform.WINDOWS:
+        APPDATA = os.
+        ln('~/dotfiles/special/vscode/User', f'{APPDATA}/Code/User')
+        ln('~/Games/Factorio', f'{APPDATA}/Factorio')
+        ln('~/Programs/Everything', f'{APPDATA}/Everything')
+
+    ln(f'{PROJ}/unity-meta', '~/unity-meta')
+
+if __name__ == "__main__":
+    pytest.main(__file__)
+"""
