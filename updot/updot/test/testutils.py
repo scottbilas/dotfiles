724d9aee23f786d64a158e118bc25cfcb8163f54
Scott Bilas <scottbilas@gmail.com>
2018-02-04 18:30:39 +0100

    Removing the environment mocker, which I must have added before I discovered pytest and `monkeypatch`, which supports env mocking

--- updot/updot/test/testutils.py
+++ updot/updot/test/testutils.py
@@ -6,40 +6,6 @@ import types
 HOME = os.path.expanduser('~').replace('\\', '/')
 
 
-# adapted from https://stackoverflow.com/a/34333710/14582
-@contextlib.contextmanager
-def modified_environ(*remove, **update):
-    """
-    Temporarily updates the ``os.environ`` dictionary in-place.
-
-    The ``os.environ`` dictionary is updated in-place so that the modification
-    is sure to work in all situations.
-
-    :param remove: Environment variables to remove.
-    :param update: Dictionary of environment variables and values to add/update.
-    """
-    env = os.environ
-    update = update or {}
-    remove = remove or []
-
-    # List of environment variables being updated or removed.
-    stomped = (set(update.keys()) | set(remove)) & set(env.keys())
-    # Environment variables and values to restore on exit.
-    update_after = {k: env[k] for k in stomped}
-    # Environment variables and values to remove on exit.
-    remove_after = frozenset(k for k in update if k not in env)
-
-    try:
-        env.update(update)
-        for item in remove:
-            env.pop(item, None)
-        yield
-    finally:
-        env.update(update_after)
-        for item in remove_after:
-            env.pop(item)
-
-
 # only updot main api funcs will automatically do tilde-expansion, so manually expand for everything else
 def expand(path):
     exp = HOME + path[1:] if path[0] == '~' else path
