9bedec33f565a156039a7ed586756d49b908aaf7
Scott Bilas <scottbilas@gmail.com>
2019-02-05 09:57:19 +0100

    Fix errors from `realias` and update `free` to do all drives

--- config/posh/aliases.ps1
+++ config/posh/aliases.ps1
@@ -5,10 +5,16 @@ function realias {
     . "$($ProfileVars.ProfileRoot)\aliases.ps1"
 }
 
+function del-if-exists($path) {
+    if (test-path $path) {
+        del $path
+    }
+}
+
 # these aliases only cause problems
-del alias:curl -fo -ea silent
-del alias:wget -fo -ea silent
-del alias:diff -fo -ea silent
+del-if-exists alias:curl
+del-if-exists alias:wget
+del-if-exists alias:diff
 
 # we never want to use `more` as a pager (and some things use it by default, like `help`)
 set-alias more less
@@ -19,7 +25,12 @@ function l { get-childitemcolorformatwide $args }
 function ll { dir -fo $args }
 function ~ { cd ~ }
 
-del alias:cls -fo -ea silent
+# https://stackoverflow.com/a/1663623/14582
+
+function free {gdr -psp 'FileSystem'}
+
+# this resets conemu to start printing ansi colors again (https://conemu.github.io/en/AnsiEscapeCodes.html#Example_3_scroll_console_to_bottom)
+del-if-exists alias:cls
 function cls { "$([char]0x1b)c$([char]0x1b)[9999H" }
 
 function theme {
