d9620b18b479be1eabaa3dd8bf77e8e7721b4d55
Scott Bilas <scottbilas@gmail.com>
2019-02-05 11:10:01 +0100

    Fix aliases for good

--- config/posh/aliases.ps1
+++ config/posh/aliases.ps1
@@ -5,16 +5,18 @@ function realias {
     . "$($ProfileVars.ProfileRoot)\aliases.ps1"
 }
 
-function del-if-exists($path) {
-    if (test-path $path) {
-        del $path
+function nuke-alias($name) {
+    # remove from every scope
+    # https://stackoverflow.com/a/24743647/14582
+    while (test-path alias:$name) {
+        del -force alias:$name
     }
 }
 
 # these aliases only cause problems
-del-if-exists alias:curl
-del-if-exists alias:wget
-del-if-exists alias:diff
+nuke-alias curl
+nuke-alias wget
+nuke-alias diff
 
 # we never want to use `more` as a pager (and some things use it by default, like `help`)
 set-alias more less
@@ -30,7 +32,7 @@ function ~ { cd ~ }
 function free {gdr -psp 'FileSystem'}
 
 # this resets conemu to start printing ansi colors again (https://conemu.github.io/en/AnsiEscapeCodes.html#Example_3_scroll_console_to_bottom)
-del-if-exists alias:cls
+nuke-alias cls
 function cls { "$([char]0x1b)c$([char]0x1b)[9999H" }
 
 function theme {
