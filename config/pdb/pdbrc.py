b726df9df0e128e25833bdbfa818412a38332f41
Scott Bilas <scottbilas@gmail.com>
2021-09-05 16:32:25 +0200

    Migrated to chezmoi

--- config/pdb/pdbrc.py
+++
@@ -1,8 +0,0 @@
-from pdb import DefaultConfig
-
-
-class Config(DefaultConfig):
-    """Config options for `pdb++ <https://pypi.python.org/pypi/pdbpp/>`_"""
-
-    prompt = ":P "
-    sticky_by_default = True
